eb965fb1b3b77e2a499cff2a81460de0aa763dc3
Laurie <github@seldo.com>
2013-07-02 23:48:50 -0700

    Source utils need the ability to parse, modify and re-output the view code

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
  * Utility functions for working with makomi project source structures
  */
 
-var fs = require('fs');
-exports.definition;
-exports.routes;
+var fs = require('fs-extra'),
+  htmlparser = require('htmlparser');
 
 /**
  * Where to find various important files. Always the same.
@@ -17,6 +16,8 @@ var constants = {
   }
 }
 
+exports.definition;
+exports.routes;
 exports.srcDir = constants.srcDir
 
 /**
@@ -48,5 +49,77 @@ exports.loadRoutes = function(sourceDir,cb) {
     exports.routes = JSON.parse(data)
     cb(exports.routes)
   });
+
+}
+
+/**
+ * Prepare a source directory for editing by makomi.
+ * This primarily means adding IDs to everything so the editor
+ * knows how to edit things.
+ * @param appDefinition
+ * @param sourceDir
+ * @param outputDir
+ * @param cb
+ */
+exports.generateWorkingCopy = function(appDefinition,sourceDir,outputDir,cb) {
+
+  var scratchSource = outputDir + '.makomi/'
+  var scratchApp = outputDir + 'app/'
+
+  // pre-process the source to add IDs
+  fs.copy(sourceDir,scratchSource,function(){
+    exports.idify(scratchSource,function(idMap) {
+      mkEx.generate(scratchSource,scratchApp,"all",function() {
+        // send the source map back so peeps can use it
+        cb(idMap)
+      })
+    })
+  })
+}
+
+/**
+ * Read every template file in the views directory
+ * Add a unique ID to the makomi-id attribute of every tag
+ *
+ * @param scratchSource
+ * @param cb
+ */
+exports.idify = function(scratchSource,cb) {
+  var viewDir = scratchSource + 'views/'
+
+  var recursiveModify = function(dir,cb) {
+    fs.readdir(dir,function(er,files) {
+
+      // TODO: build a tree
+
+      var count = files.length
+      var complete = function() {
+        count--
+        if (count == 0) cb()
+      }
+
+      files.forEach(function(file) {
+        fs.stat(dir+'/'+file,function(er,stats) {
+          if(stats.isDirectory()) {
+            // it's a folder, so recurse
+            recursiveModify(dir+'/'+file,function() {
+              complete()
+            })
+          } else {
+            // it's a file, so parse and modify
+            parseFile(dir+'/'+file,function(dom) {
+              addIds(dom,function(newDom) {
+                toHtml(newDom,function(html) {
+                  fs.writeFile(dir+'/'+file,html,function() {
+                    complete()
+                  })
+                })
+              })
+            })
+          }
+        })
+      })
+    })
+  }
 
 }
