ccd8070e65500e6244a87d0513b14c03fafdb834
Laurie <github@seldo.com>
2013-07-30 16:24:42 -0700

    Added .remove() method

---
+++ test/remove.js
@@ -0,0 +1,30 @@
+/**
+ * Remove an element (and all its children) from the tree
+ * @type {*}
+ */
+var test = require('tape');
+var mkSrc = require('../index.js');
+var testUtil = require('./util.js')
+var util = require('util')
+var htmlparser = require("htmlparser");
+
+test('remove the given element from the tree', function (t) {
+
+  t.plan(1);
+
+  var inFile = "./test/data/remove/before.html"
+  var expectedFile = "./test/data/remove/after.html"
+  var outFile = "/tmp/remove.html"
+  var targetId = "xxxxx6"
+
+  testUtil.compareToExpectedOutput(t,expectedFile,function(cb) {
+    mkSrc.parseFile(inFile,function(er,dom) {
+      mkSrc.remove(dom,targetId,function(newDom) {
+        mkSrc.writeHtml(outFile,newDom,function(html) {
+          cb(html)
+        })
+      })
+    })
+  })
+
+});
